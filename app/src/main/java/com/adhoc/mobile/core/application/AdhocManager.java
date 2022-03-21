e902dc0d5a38d4a0fa99d9cc55d813a6bb3ffa22
neroli-realy <alyyaser12@gmail.com>
2022-03-21 04:22:25 +0200

    Added Encryption/Decryption

--- app/src/main/java/com/adhoc/mobile/core/application/AdhocManager.java
+++ app/src/main/java/com/adhoc/mobile/core/application/AdhocManager.java
@@ -6,22 +6,31 @@ import com.adhoc.mobile.core.network.NetworkCallbacks;
 import com.adhoc.mobile.core.network.NetworkManager;
 import com.adhoc.mobile.core.util.User;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 public class AdhocManager {
 
     private final Context context;
     private final AdhocManagerCallbacks callbacks;
     private final NetworkManager networkManager;
     private final User user;
+    private final Security security;
 
     private NetworkCallbacks networkCallbacks = new NetworkCallbacks() {
 
     };
 
-    public AdhocManager(Context context, User user, AdhocManagerCallbacks callbacks) {
+    public AdhocManager(Context context, User user, AdhocManagerCallbacks callbacks) throws NoSuchAlgorithmException {
         this.context = context;
         this.callbacks = callbacks;
         this.user = user;
 
+        security = new Security();
         networkManager = new NetworkManager(context, networkCallbacks);
     }
 
@@ -33,10 +42,11 @@ public class AdhocManager {
         networkManager.leaveNetwork();
     }
 
-    public void sendMessage(String message, String destination) {
+    public void sendMessage(String message, String destination) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
 
+        // 1.TODO we will need to extract public key from destination
+        String encryptedMessage = security.encrypt(message, security.getPublicKey());
+        networkManager.sendMessage(encryptedMessage);
 
-        // 1.TODO(Encrypt the message)
-        // 2.TODO(Call NetworkManager.sendMessage())
     }
 }
---
+++ app/src/main/java/com/adhoc/mobile/core/application/Security.java
@@ -0,0 +1,84 @@
+package com.adhoc.mobile.core.application;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class Security {
+
+
+    private static PublicKey publicKey;
+    private static PrivateKey privateKey;
+
+    public Security() throws NoSuchAlgorithmException {
+        //generate Keys
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(1024);
+        KeyPair pair = generator.generateKeyPair();
+        privateKey = pair.getPrivate();
+        publicKey = pair.getPublic();
+    }
+    public static PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public static PrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public static void setPublicKey(PublicKey publicKey) {
+        Security.publicKey = publicKey;
+    }
+
+    public static void setPrivateKey(PrivateKey privateKey) {
+        Security.privateKey = privateKey;
+    }
+
+
+    private String BytestoHex(byte[] bytes){
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes)
+            sb.append(String.format("%02X", b));
+        return sb.toString();
+    }
+
+    private byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+    public String encrypt(String Message, PublicKey publicKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher encryptCipher = Cipher.getInstance("RSA");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] secretMessageBytes = Message.getBytes(StandardCharsets.UTF_8);
+        byte[] encryptedMessageBytes = encryptCipher.doFinal(secretMessageBytes);
+        return BytestoHex(encryptedMessageBytes);
+    }
+
+
+    public String decrypt(String Message, PrivateKey privateKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher decryptCipher = Cipher.getInstance("RSA");
+        decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] encryptedMessageBytes = hexStringToByteArray(Message);
+        byte[] decryptedMessageBytes = decryptCipher.doFinal(encryptedMessageBytes);
+        String decryptedMessage = new String(decryptedMessageBytes, StandardCharsets.UTF_8);
+        return  decryptedMessage;
+    }
+}
